4f54bf166c2e10d40333df1fe23f4597fcb8017d
DaJaime <jaime.damien@gmail.com>
2024-09-11 20:04:10 +0200

    Add API swagger doc, Fixes #3

--- app/src/main/java/fr/parisnanterre/controllers/HelloController.java
+++ app/src/main/java/fr/parisnanterre/controllers/HelloController.java
@@ -1,12 +1,19 @@
 package fr.parisnanterre.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Hello Controller")
 public class HelloController {
 
     @GetMapping("/hello")
+    @Operation(
+            summary = "Returns a welcome message",
+            description = "This endpoint returns a simple message saying 'Hello from API'"
+    )
     public String getHello() {
         return "Hello from API";
     }
